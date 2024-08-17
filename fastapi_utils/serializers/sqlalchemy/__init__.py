6dc4746741d6268e13c28c94a342a7de7e9aa702
zayycev22 <zayycev22@gmail.com>
2024-08-18 01:03:02 +0300

    added __all__

---
+++ fastapi_utils/serializers/sqlalchemy/__init__.py
@@ -0,0 +1,3 @@
+from fastapi_utils.serializers.sqlalchemy.serializers import ModelSerializer
+
+__all__ = ['ModelSerializer']
---
+++ fastapi_utils/serializers/tortoise/__init__.py
@@ -0,0 +1,3 @@
+from fastapi_utils.serializers.tortoise.serializers import ModelSerializer
+
+__all__ = ['ModelSerializer']
